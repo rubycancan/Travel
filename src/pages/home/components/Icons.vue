6a90acac9936cfdb9c71af53ab801d7a618aff98
rubycancan <249891289@qq.com>
2019-10-08 17:13:57 +0800

    'addRecommend'

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -91,36 +91,38 @@ export default {
     /*width: 100%*/
     /*height: 0*/
     /*padding-bottom: 50%*/
-  .icon
-    position: relative
-    overflow: hidden
-    float: left
-    width: 25%
-    height: 0
-    padding-bottom: 25%
-    .icon-img
-      position: absolute
-      top: 0
-      left: 0
-      right: 0
-      bottom: .44rem
-      box-sizing:border-box
-      padding: .05rem
-      .icon-image-content
-        display: block
-        margin: 0 auto
-        height: 100%
-    .icon-desc
-      position: absolute
-      left: 0
-      right: 0
-      bottom: .1rem
-      height: .44rem
-      line-height: .44rem
-      text-align: center
-      color: $darkTextColor
+  .icons
+    margin-top: .2rem
+    .icon
+      position: relative
       overflow: hidden
-      white-space: nowrap
-      text-overflow: ellipsis
-      ellipsis()
+      float: left
+      width: 25%
+      height: 0
+      padding-bottom: 25%
+      .icon-img
+        position: absolute
+        top: 0
+        left: 0
+        right: 0
+        bottom: .44rem
+        box-sizing:border-box
+        padding: .05rem
+        .icon-image-content
+          display: block
+          margin: 0 auto
+          height: 100%
+      .icon-desc
+        position: absolute
+        left: 0
+        right: 0
+        bottom: .1rem
+        height: .44rem
+        line-height: .44rem
+        text-align: center
+        color: $darkTextColor
+        overflow: hidden
+        white-space: nowrap
+        text-overflow: ellipsis
+        ellipsis()
 </style>
